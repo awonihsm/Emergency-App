f4641db153c791bffa4125edb9802b2e551b1d05
awonihsm <25raytong@gmail.com>
2023-06-12 08:45:37 -0700

    Initial Commit

---
+++ emergencyApp/emergencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  emergencyAppApp.swift
+//  emergencyApp
+//
+//  Created by Ray Tong on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct emergencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
